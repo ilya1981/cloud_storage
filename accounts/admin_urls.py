14646435ecafbbab9b677c15c0076e8b0a277f56
ilya1981 <80913864+ilya1981@users.noreply.github.com>
2026-08-08 20:21:37 +0300

    Update admin_urls.py

--- accounts/admin_urls.py
+++ accounts/admin_urls.py
@@ -1,1 +1,7 @@
+from django.urls import path
+from .views import AdminUsersListView, AdminUserDetailView
 
+urlpatterns = [
+    path('users/', AdminUsersListView.as_view(), name='admin-users-list'),
+    path('users/<int:pk>/', AdminUserDetailView.as_view(), name='admin-user-detail'),
+]
